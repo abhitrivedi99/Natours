319d5ca60d9afe0d14bc52e53bf4311df90e7107
Abhishek Trivedi <abhitrivedi99@gmail.com>
2020-07-07 13:45:41 +0530

    app:

--- app.js
+++ app.js
@@ -15,16 +15,19 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello middleware');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server`,
+  });
+  next();
+});
 
 module.exports = app;
